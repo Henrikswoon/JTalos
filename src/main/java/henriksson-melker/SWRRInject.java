0aa2900c06d5827480204a5c353b178c23542adc
Melker Henriksson <melhen12344@gmail.com>
2025-04-04 16:12:07 +0200

    Implemented OPTIONS, allowing jtalos to read SWRR_OPTIONS at project root to determine which methods should be instrumented.

--- src/main/java/henriksson-melker/SWRRInject.java
+++ src/main/java/henriksson-melker/SWRRInject.java
@@ -4,9 +4,14 @@ import soot.*;
 import soot.jimple.Jimple;
 import soot.jimple.NewExpr;
 import soot.jimple.SpecialInvokeExpr;
+import soot.jimple.StringConstant;
 
+import java.io.*;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SWRRInject {
     static Jimple jimple = Jimple.v();
@@ -16,6 +21,30 @@ public class SWRRInject {
     static Unit throwStmt;
     public static void main(String[] args){
         System.out.println("Entered JTalos...");
+        String[] SWRR_OPTIONS;
+        try (FileReader fr = new FileReader("SWRR_OPTIONS");
+             BufferedReader br = new BufferedReader(fr)){
+
+            List<String> lines = br.lines().toList();
+            SWRR_OPTIONS = lines.toArray(new String[0]);
+
+            int i = 0;
+            String option = br.readLine();
+            while(option != null){
+                SWRR_OPTIONS[i] = option;
+                option = br.readLine();
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("SWRR_OPTIONS not found, if this is missing please create one in the jtalos root directory...");
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to access SWRR_OPTIONS, see error:\n" + e);
+        }
+        System.out.println("Options read into memory...");
+
+        for (int j = 0; j < SWRR_OPTIONS.length; j++)
+            System.out.println("\t " + SWRR_OPTIONS[j]);
+
         PackManager.v().getPack("wjtp").add(
                 new Transform("wjtp.JTalos", new SceneTransformer() {
                     protected void internalTransform(String phaseName,
@@ -47,12 +76,16 @@ public class SWRRInject {
                                 System.out.println("  "+ sm);
                                 try{
                                     Body b = sm.retrieveActiveBody();
-                                    b.getLocals().addFirst(r1);
-                                    PatchingChain<Unit> units = b.getUnits();
-                                    Unit head = units.getFirst();
-                                    units.insertBefore(assignStmt, head);
-                                    units.insertAfter(invokeStmt, assignStmt);
-                                    units.insertAfter(throwStmt, invokeStmt);
+                                    String uid = sc.getName() + "." + sm.getName();
+                                    if(Arrays.asList(SWRR_OPTIONS).contains(uid)){
+                                        System.out.println("Instrumenting " + uid + " ...");
+                                        b.getLocals().addFirst(r1);
+                                        PatchingChain<Unit> units = b.getUnits();
+                                        Unit head = units.getFirst();
+                                        units.insertBefore(assignStmt, head);
+                                        units.insertAfter(invokeStmt, assignStmt);
+                                        units.insertAfter(throwStmt, invokeStmt);
+                                    }
                                     System.out.println("Jimple body: "+ b);
                                 } catch (RuntimeException e){
                                     System.err.println(e);
@@ -60,10 +93,9 @@ public class SWRRInject {
                             }
                         }
                     }
-
                 }));
 
         System.out.println("Finished adding Pack:jtalos");
-        soot.Main.main(args);
+        Main.main(args);
     }
 }
